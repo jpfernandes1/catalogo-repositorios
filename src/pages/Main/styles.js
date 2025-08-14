1a353d3a8d8a72bcc3570e9c97882c4da4fb2b91
jpfernandes1 <jp_fernandes1@live.com>
2025-08-14 16:50:06 -0300

    style: style of the add repository button

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, {keyframes, css} from 'styled-components';
 
 
 export const Container = styled.div`
@@ -37,7 +37,20 @@ export const Form = styled.form`
 
 `;
 
-export const SubmitButton = styled.button`
+// Button Animation
+const animate = keyframes`
+    from{
+    transform: rotate(0deg)
+    }
+    to{
+    transform: rotate(360deg);
+    }
+`;
+
+export const SubmitButton = styled.button.attrs(props => ({
+    type: 'submit',
+    disabled: props.loading,
+}))`
 
     background: #0D2636;
     border: 0;
@@ -47,5 +60,18 @@ export const SubmitButton = styled.button`
     display: flex;
     justify-content: center;
     align-items: center;
+
+    &[disabled]{
+        cursor: not-allowed;
+        opacity: 0.5;
+    }
+
+    ${props => props.loading && 
+        css`
+            svg{
+                animation: ${animate} 2s linear inifinite
+            }
+        `
+    }
 
 `;
