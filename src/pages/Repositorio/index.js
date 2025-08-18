47ec1772f71ffbd865b2337caa944ab880bc9284
jpfernandes1 <jp_fernandes1@live.com>
2025-08-18 15:08:44 -0300

    feat: implements function to filter obtained data anda save it locally and implements visualization of the data

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,7 +1,65 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import { Container, Owner, Loading, Titulo, Paragrafo, BackButton } from './style';
+import api from "../../services/api";
+import {FaArrowLeft} from 'react-icons/fa';
+import { useParams } from "react-router-dom";
 
-export default function Repositorio(){
+
+export default function Repositorio() {
+
+  const [repositorioData, setRepositorioData] = useState({}); // Para guardar os dados vindos da API
+  const [issues, setIssues] = useState([]);
+  const[loading, setLoading] = useState(true);
+
+  const { repositorio } = useParams(); // Parametros da URL
+
+  useEffect(() => {
+    async function load(){
+      const nomeRepo = decodeURIComponent(repositorio);;
+
+      const [repoResponse, issuesResponse] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5
+          }
+        })
+      ]);
+
+      setRepositorioData(repoResponse.data);
+      setIssues(issuesResponse.data);
+      setLoading(false);
+
+    }
+    if (repositorio) {
+      load();
+    }
+  }, [repositorio]);
+
+  if(loading){
     return(
-        <h1>Placeholder</h1>
-    )
+    <Loading>
+      <Titulo>Carregando...</Titulo>
+    </Loading>
+
+  )}
+
+return (
+  <Container>
+    <BackButton to="/">
+      <FaArrowLeft color="#000" size={30}/>
+    </BackButton>
+    {repositorioData?.owner && (
+        <Owner>
+        <img
+          src={repositorioData.owner.avatar_url}
+          alt={repositorioData.owner.login}
+        />
+        <Titulo>{repositorioData.name}</Titulo>
+        <Paragrafo>{repositorioData.description}</Paragrafo>
+      </Owner>
+    )}
+  </Container>
+);
 }
